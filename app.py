24fdd72233429be04eb25369e1336885517d7087
Ryan Gordon <ryangordon210@gmail.com>
2018-07-14 22:44:45 +0100

    Added boilerplate files

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from injector import Binder
+from flask_injector import FlaskInjector
+from connexion.resolver import RestyResolver
+import connexion
+from services.ElasticProvider import ElasticProvider
+
+# Connexion can only receive 2 params
+application = connexion.App(__name__, specification_dir='swagger/')
+
+#Setup with Swagger and RestyResolver
+application.add_api('elastic-crud-openapi.yaml', resolver=RestyResolver('api'))
+def configure(binder: Binder) -> Binder:
+    binder.bind(
+        RedisProvider,
+        RedisProvider([{"Name": "Test 1"}])
+    )
+#FlaskInjector Setup defined after configure
+FlaskInjector(app=application.app, modules=[configure])
+
+if __name__ == '__main__':
+    
+    #Redefine application as a connexion app. 
+    application = connexion.App(__name__, specification_dir='swagger/')
+    #Start the flask server on either a predefined port from the host machine or 2025
+    application.run(port=int(os.environ.get('PORT', 2025)))
