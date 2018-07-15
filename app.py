d4112456d58081e13e155ad6871764af6ef9b07b
Ryan Gordon <ryangordon210@gmail.com>
2018-07-15 18:15:39 +0100

    Set up flask_injector bindings

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
 from injector import Binder
 from flask_injector import FlaskInjector
-from connexion.resolver import RestyResolver
-import connexion
 from services.ElasticProvider import ElasticProvider
+import connexion
+from connexion.resolver import RestyResolver
+
 
 # Connexion can only receive 2 params
 #application = connexion.App(__name__, specification_dir='swagger/')
 
 #Setup with Swagger and RestyResolver
-
 def configure(binder: Binder) -> Binder:
     binder.bind(
-        ElasticProvider
+        ElasticProvider, to=ElasticProvider(host = "http://localhost:9200")  
     )
 
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     #Redefine application as a connexion app. 
     application = connexion.App(__name__, specification_dir='swagger/')
 
-    application.add_api('elastic-crud-openapi.yaml', resolver=RestyResolver('api'))
+    application.add_api('elastic-crud-openapi.yaml', resolver=RestyResolver('api'), arguments={'title': 'ElasticSearch OpenAPI'})
 
     #FlaskInjector Setup defined after configure
     FlaskInjector(app=application.app, modules=[configure])
