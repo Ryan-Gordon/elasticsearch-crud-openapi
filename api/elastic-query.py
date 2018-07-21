03abd863e27fc11760ae7f054d40b97cf4fa975d
Ryan Gordon <ryangordon210@gmail.com>
2018-07-21 12:48:47 +0100

    Non required params for query routes
    These params aren't needed to run a query so they are setup as 'None' if the user doesnt provide them.

--- api/elastic-query.py
+++ api/elastic-query.py
@@ -10,12 +10,14 @@ ElasticSearch -- Query Operations
 searchDocument -- Searches the instance looking for documents that match the provided query and RETURNS them
 
 deleteByQuery -- Searches the instance looking for documents that match the provided query and DELETES them
+
+Note: The default 'required' arguements such as body/query must come before those which are not required such as index or type
 '''
 
 @inject(data_provider=ElasticProvider)
-def searchDocument(data_provider,index, type, body) -> str:
+def searchDocument(data_provider, body, index=None, type=None) -> str:
     return data_provider.searchDocument(index, type, body)
 
 @inject(data_provider=ElasticProvider)
-def deleteByQuery(data_provider,index, type, query) -> str:
+def deleteByQuery(data_provider, query, index=None, type=None) -> str:
     return data_provider.deleteByQuery(index, type, query)
